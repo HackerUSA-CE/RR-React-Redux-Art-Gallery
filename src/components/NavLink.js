27a60fc513188abcede2975f78b9075e51c2d674
Rachel Manning <rachy@users.noreply.github.com>
2022-03-13 23:24:19 -0700

    Update NavLink.js

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-const NavLink = (props) => {
+function NavLink(props) {
     const mode = useSelector((state) => state.mode)
 
     return (
