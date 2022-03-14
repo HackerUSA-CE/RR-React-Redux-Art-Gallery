c63250e905e1e7527b570ecf02db08e08b0feb9f
Rachel Manning <rachy@users.noreply.github.com>
2022-03-13 23:20:49 -0700

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavLink from './NavLink'
 
-const Nav = () => {
+function Nav() {
 
     return (
         <div className="Nav" style={{'backgroundColor': 'white'}}>
