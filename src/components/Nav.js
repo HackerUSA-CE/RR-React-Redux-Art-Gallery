30a31393c628aaa300a57b4dd91e91b52c40a612
Rachel Manning <rachy@users.noreply.github.com>
2022-03-13 23:23:53 -0700

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import NavLink from './NavLink'
 
-const Nav = () => {
+function Nav() {
     const mode = useSelector((state) => state.mode)
 
     return (
